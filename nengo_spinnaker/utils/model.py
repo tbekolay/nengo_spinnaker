e0ff1696c3d15172f380dc6b0ed2d246dc227908
AndrewMundy <andrew.mundy@ieee.org>
2015-06-10 16:00:14 -0400

    WIP Merge trees

--- nengo_spinnaker/utils/model.py
+++ nengo_spinnaker/utils/model.py
@@ -2,10 +2,17 @@
 """
 from __future__ import absolute_import
 
+from collections import defaultdict
 import itertools
-from six import iteritems
+import nengo.synapses
+from six import iteritems, itervalues
 
 from nengo_spinnaker.operators import Filter
+
+MAX_FAN_IN = 8
+"""The maximum number of signals which will be allowed to terminate on any
+given object.
+"""
 
 
 def remove_sinkless_signals(model):
@@ -84,3 +91,131 @@ def remove_childless_filters(model):
         # signals which have no sinks, we should remove these as it will allow
         # us to find further filters with no children.
         remove_sinkless_signals(model)
+
+
+def _get_possible_merges(model, max_fan_in=MAX_FAN_IN):
+    """Get a list of signals which could be merged in merge trees to reduce the
+    fan-ins in the model.
+
+    Returns
+    -------
+    [(object, [signal, ...]), ...]
+        Pairs of objects with respective incoming signals which could be merged
+        to reduce the fan in.
+    """
+    # Build up a dict {obj: {port: {filter: [signals, ...]}}} and a dict {obj:
+    # fan_in} which will allow us to identify cases where merge trees are
+    # necessary and which signals may be merged.
+    opfs = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    obj_fanin = defaultdict(lambda: 0)
+
+    # Create an iterator of all signals combined with related connections, or
+    # None if there is no associated connection.
+    def all_signals_and_connections(model):
+        # Build a dictionary which maps signals to all of the connections with
+        # which they are associated. Signals from the list of extra signals are
+        # associated with the empty list.
+        signal_connections = defaultdict(list)
+
+        # All signals associated with connections
+        for conn, sigs in iteritems(model.connections_signals):
+            for sig in sigs:
+                signal_connections[sig].append(conn)
+
+        # All other signals
+        for sig in model.extra_signals:
+            # Accessing the key is enough to ensure that it exists
+            signal_connections[sig]
+
+        # Now we just return an iterator over this dictionary, this will yield
+        # (signal, [connection, ...]) pairs.
+        return iteritems(signal_connections)
+
+    # For every (signal, connections) pair in the model increment the fan-in of
+    # all its sinks and include the signal in the dictionary of possible
+    # merges.
+    for signal, connections in all_signals_and_connections(model):
+        # For every sink in the signal we perform the same processing
+        for sink in signal.sinks:
+            # First we increment the fan in to the sink object
+            obj_fanin[sink.obj] += 1
+
+        # Now create a hashable representation of the synapse, if we can't then
+        # we just skip to the next signal.
+        if len(connections) == 0:
+            # We treat no connections as if there were a `None` synapse
+            synapse = None
+        elif len(connections) == 1:
+            # We grab a representation of the synapse as a tuple and use this
+            conn = connections[0]
+            if conn.synapse is None:
+                synapse = conn.synapse
+            elif isinstance(conn.synapse, nengo.synapses.LinearFilter):
+                synapse = (type(conn.synapse),
+                           tuple(conn.synapse.num),
+                           tuple(conn.synapse.den))
+            else:
+                raise NotImplementedError("Unknown synapse type")
+        else:
+            # It's not clear what we should do, so we do nothing
+            continue
+
+        # Now we include the signal in the dictionary of possible merges
+        for sink in signal.sinks:
+            opfs[sink.obj][sink.port][synapse].append(signal)
+
+    # To finish off we return a list of the objects with a fan-in greater than
+    # the maximum fan-in and their possible merges.
+    return list(
+        # Pairs of objects and signals, extracted by ignoring ports and filters
+        # in the large dictionary we created above.
+        (obj, sigs) for obj, pfs in iteritems(opfs)
+        for fs in itervalues(pfs) for sigs in itervalues(fs)
+        # Provided there's more than 1 signal and the object has a fan-in
+        # greater than the max we include this list of signals and the object.
+        if len(sigs) > 1 and obj_fanin[obj] > max_fan_in
+    )
+
+
+def _get_equal_merges(n_in, max_fan_in=MAX_FAN_IN):
+    """Generate slices which can be used to collect signals into merge nodes.
+
+    Parameters
+    ----------
+    n_in : int
+        Number of signals to combine.
+    max_fan_in : int
+        Maximum allowed fan in.
+    """
+    # Special case, we have less than the maximum fan in, just yield
+    if n_in <= max_fan_in:
+        yield slice(0, n_in)
+    else:
+        # Calculate the remainder, then split the missing items over two
+        # entries
+        n = 0
+        r = n_in % max_fan_in
+        if r != 0:
+            r += max_fan_in
+            yield slice(0, (r // 2))
+            yield slice((r // 2), (r // 2) + -(-r // 2))
+
+        # Yield the remaining merge lengths
+        n = r
+        n_in -= r
+        while n_in > 0:
+            # Next slice
+            length = min((max_fan_in, n_in))
+            yield slice(n, n+length)
+            n_in -= length
+            n += length
+
+
+def _insert_merge_tree(model, obj, signals, max_fan_in=MAX_FAN_IN):
+    """Insert a signal merge tree into the model.
+
+    This will insert new operators into the model, will disconnect each of the
+    listed signals from the given object and attach them to the merge objects
+    and will connect the merge objects to the original object.  This will
+    happen repeatedly until the merge tree obeys the max fan in requirement.
+    """
--- tests/utils/test_utils_model.py
+++ tests/utils/test_utils_model.py
@@ -1,9 +1,13 @@
 import mock
+import nengo
+import pytest
 
 from nengo_spinnaker.builder.builder import Model, ObjectPort, Signal
 from nengo_spinnaker.operators import Filter
-from nengo_spinnaker.utils.model import (remove_childless_filters,
-                                         remove_sinkless_signals)
+from nengo_spinnaker.utils.model import (
+    remove_childless_filters, remove_sinkless_signals, _get_possible_merges,
+    _get_equal_merges, _insert_merge_tree
+)
 
 
 def test_remove_sinkless_signals():
@@ -109,3 +113,150 @@ def test_remove_childless_filters():
     assert model.connections_signals == {}
     assert model.extra_signals == [s1, s2, s3]
     assert [s.obj for s in s3.sinks] == [o2]
+
+
+def test_get_possible_merges():
+    """Test that we correctly identify sets of signals which can merged
+    together to reduce fan-in.
+
+        (filter 1, latching)-[s0]->\
+              ----(filter 1)-[s1]->|
+              ----(filter 1)-[s2]->+--> (o1)
+              ----(filter 2)-[s3]->|
+              ----(filter 2)-[s4]->|
+        --(filter 2, port b)-[s5]->/
+
+              ----(filter 1)-[s6]->\
+              ----(filter 1)-[s7]->+--> (o2)
+              ----(latching)-[s8]->/
+
+    We should identify that the signals into `o1` require merging but that only
+    (`s0`, `s1` and `s2`) and (`s3` and `s4`) can actually be merged.
+    """
+    # Create the objects
+    o1 = mock.Mock()
+    o2 = mock.Mock()
+
+    # Create the filters
+    filter1 = nengo.Lowpass(0.005)
+    filter2 = nengo.Lowpass(0.03)
+
+    # Create the signals
+    s0 = Signal(ObjectPort(None, None), ObjectPort(o1, None), None,
+                latching=True)
+    s1 = Signal(ObjectPort(None, None), ObjectPort(o1, None), None)
+    s2 = Signal(ObjectPort(None, None), ObjectPort(o1, None), None)
+    s3 = Signal(ObjectPort(None, None), ObjectPort(o1, None), None)
+    s4 = Signal(ObjectPort(None, None), ObjectPort(o1, None), None)
+    s5 = Signal(ObjectPort(None, None), ObjectPort(o1, mock.Mock()), None)
+
+    s6 = Signal(ObjectPort(None, None), ObjectPort(o2, None), None)
+    s7 = Signal(ObjectPort(None, None), ObjectPort(o2, None), None)
+    s8 = Signal(ObjectPort(None, None), ObjectPort(o2, None), None,
+                latching=True)
+
+    # Create some connections with which to associate the signals
+    c1 = mock.Mock(spec=nengo.Connection)
+    c1.synapse = filter1
+    c2 = mock.Mock(spec=nengo.Connection)
+    c2.synapse = filter2
+    c3 = mock.Mock(spec=nengo.Connection)
+    c3.synapse = filter1
+
+    # Construct the model
+    model = Model()
+    model.connections_signals[c1] = [s0, s1, s2, s6, s7]
+    model.connections_signals[c2] = [s3, s4, s5]
+    model.extra_signals.append(s8)
+
+    # Get the possible merges, check they are valid
+    merges = _get_possible_merges(model, max_fan_in=3)
+    assert len(merges) == 2
+    for (obj, sigs) in merges:
+        assert obj is o1
+        assert set(sigs) in set([frozenset([s0, s1, s2]), frozenset([s3, s4])])
+
+
+@pytest.mark.parametrize(
+    "n_in, max_fan_in, expected",
+    [(4, 4, [slice(0, 4)]),
+     (2, 4, [slice(0, 2)]),
+     (3, 4, [slice(0, 3)]),
+     (5, 4, [slice(0, 2), slice(2, 5)]),
+     (6, 4, [slice(0, 3), slice(3, 6)]),
+     (7, 4, [slice(0, 3), slice(3, 7)]),
+     (9, 4, [slice(0, 2), slice(2, 5), slice(5, 9)]),
+     (10, 4, [slice(0, 3), slice(3, 6), slice(6, 10)]),
+     (11, 4, [slice(0, 3), slice(3, 7), slice(7, 11)]),
+     ]
+)
+def test_get_equal_merges(n_in, max_fan_in, expected):
+    assert list(_get_equal_merges(n_in, max_fan_in)) == expected
+
+
+def test_insert_merge_tree():
+    """Tests that a merge tree is correctly inserted for some specified
+    signals.
+    
+    We construct the model:
+
+        --- (c1 => s0) ----\--> o2
+        --- (c1 => s1) ----|
+        --- (c1 => s2) ----+--> o1
+        --- (c1 => s3) ----|
+        --- (   => s4) ----|
+        --- (c2 => s5) --- /
+
+    And check that with a maximum fan in of 3 and specifying all (c1) signals
+    we create:
+
+        --- (c1 => s0) ----\-------------> o2
+        --- (c1 => s1) -------> m1 --\
+                                      +--> o1
+        --- (c1 => s2) ----\          | 
+        --- (c1 => s3) ----+--> m2 ---+
+        --- (   => s4) ----/          |
+                                      |
+        --- (c2 => s5) --------------/  
+    """
+    # Create the terminating object
+    o1 = mock.Mock(name="o1")
+    o2 = mock.Mock(name="o2")
+
+    # Create all the signals
+    ss = [Signal(ObjectPort(None, None), ObjectPort(o1, None), None) for _ in
+          range(6)]
+    ss[0].sinks.append(ObjectPort(o2, None))
+
+    # Create two connections
+    c1 = mock.Mock(name="c1")
+    c2 = mock.Mock(name="c2")
+
+    # Create the model
+    model = Model()
+    model.connections_signals[c1] = ss[0:4]
+    model.extra_signals.append(ss[4])
+    model.connections_signals[c2] = [ss[5]]
+
+    # Insert a merge tree
+    _insert_merge_tree(model, o1, ss[0:5], max_fan_in=3)
+
+    # Check that merge nodes were inserted
+    assert len(model.extra_operators) == 2  # 2 merge nodes
+
+    # Check that some extra signals were inserted
+    assert len(model.connections_signals[c1]) == 8
+    assert len(model.extra_signals) == 2
+    assert len(model.connections_signals[c2]) == 1  # Unchanged
+
+    # Check that each of the signals associated with c1 is either a signal we
+    # had before, or that it is a new signal with appropriate start and end
+    # points.
+    for sig in model.connections_signals[c1]:
+        if sig is ss[0]:
+            assert o2 in [s.obj for s in sig.sinks]
+        elif sig not in ss:
+            assert sig.source.obj in model.extra_operators
+            assert [o1] == [s.obj for s in sig.sinks]
+        else:
+            assert sig in ss
